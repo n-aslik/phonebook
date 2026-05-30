cfe0b68bc5657297567e89d4b739dc7df156bdca
asliddin <aslnabiev2002@gmail.com>
2026-05-30 10:28:49 +0500

    third commit

--- phonebook/wsgi.py
+++ phonebook/wsgi.py
@@ -13,4 +13,12 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phonebook.settings')
 
-application = get_wsgi_application()
+# Автоматический collectstatic при запуске на Vercel
+try:
+    from django.core.management import call_command
+    call_command('collectstatic', '--noinput', '--clear')
+except Exception as e:
+    print(f"Error running collectstatic: {e}")
+
+app = get_wsgi_application() # Vercel ищет переменную app или application
+application = app
